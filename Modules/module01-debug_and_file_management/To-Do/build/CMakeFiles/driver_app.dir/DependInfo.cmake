e47ab8af7a79977c4d9940764fc0503263b96bd8
DaivikNeogi <daivikneogi1@gmail.com>
2026-06-21 10:50:18 +0000

    Reverted folder structure changes; restored original file paths

---
+++ Modules/module01-debug_and_file_management/To-Do/build/CMakeFiles/driver_app.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/devix/workspace/RTOS-and-Embedded-System-Design-Task-/Modules/module01-debug_and_file_management/To-Do/src/battery.c" "CMakeFiles/driver_app.dir/src/battery.c.o" "gcc" "CMakeFiles/driver_app.dir/src/battery.c.o.d"
+  "/home/devix/workspace/RTOS-and-Embedded-System-Design-Task-/Modules/module01-debug_and_file_management/To-Do/src/fake_mcu.c" "CMakeFiles/driver_app.dir/src/fake_mcu.c.o" "gcc" "CMakeFiles/driver_app.dir/src/fake_mcu.c.o.d"
+  "/home/devix/workspace/RTOS-and-Embedded-System-Design-Task-/Modules/module01-debug_and_file_management/To-Do/src/main.c" "CMakeFiles/driver_app.dir/src/main.c.o" "gcc" "CMakeFiles/driver_app.dir/src/main.c.o.d"
+  "/home/devix/workspace/RTOS-and-Embedded-System-Design-Task-/Modules/module01-debug_and_file_management/To-Do/src/sensor.c" "CMakeFiles/driver_app.dir/src/sensor.c.o" "gcc" "CMakeFiles/driver_app.dir/src/sensor.c.o.d"
+  "" "driver_app" "gcc" "CMakeFiles/driver_app.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Modules/module01-debug_and_file_management/To-Do/build/CMakeFiles/driver_app.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/driver_app.dir/link.d"
+  "CMakeFiles/driver_app.dir/src/battery.c.o"
+  "CMakeFiles/driver_app.dir/src/battery.c.o.d"
+  "CMakeFiles/driver_app.dir/src/fake_mcu.c.o"
+  "CMakeFiles/driver_app.dir/src/fake_mcu.c.o.d"
+  "CMakeFiles/driver_app.dir/src/main.c.o"
+  "CMakeFiles/driver_app.dir/src/main.c.o.d"
+  "CMakeFiles/driver_app.dir/src/sensor.c.o"
+  "CMakeFiles/driver_app.dir/src/sensor.c.o.d"
+  "driver_app"
+  "driver_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/driver_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
